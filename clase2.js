0ced0c8b4d762773ab44cb3e5df91f9eedc28c1f
juanCsacasa <110007393+juanCsacasa@users.noreply.github.com>
2023-01-28 11:48:50 -0600

    clase 2

---
+++ clase2.js
@@ -0,0 +1,17 @@
+//1. Escriba una function que escriba un número a la inversa
+let numero = 1202;
+function name(numero) {
+  for (let i = 0; i < numero.length; i--) {
+    
+  }
+}
+
+
+//2. Escriba una function que acepte un string como parámetro y convierta cada primer letra de cada palabra en mayúscula
+
+let string = "juan";
+
+function name(string) {
+  for (let i = 0; i < array.length; i++) {
+  }
+}
