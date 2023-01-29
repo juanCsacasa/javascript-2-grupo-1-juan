06a587a0ed9c256b80240eb23fb92caff4a8d8e2
juan sacasa <juanksacasa@gmail.com>
2023-01-29 00:10:40 -0600

    casi me da algo :C

--- clase2.js
+++ clase2.js
@@ -1,17 +1,30 @@
 //1. Escriba una function que escriba un número a la inversa
-let numero = 1202;
-function name(numero) {
-  for (let i = 0; i < numero.length; i--) {
-    
+function alverris(numero) {
+  let alverrisnuevo = "";
+  for (let i = numero.length - 1; i >= 0; i--) {
+    alverrisnuevo += numero[i];
   }
+  return alverrisnuevo;
 }
-
+//pense en hacer algo como este decirle a la compu que si era diferente de string lo imprimiera y sino era diferente de string pusiera que siguiera intentando pero no ssupe como realizarlo del todo una disculpa.
+/*if (alverrisnuevo !== string) {
+  console.log(alverris('hola'));
+  }else{
+    console.log(alverris('hola') + "siga echandole ganas")
+  }*/
 
 //2. Escriba una function que acepte un string como parámetro y convierta cada primer letra de cada palabra en mayúscula
 
-let string = "juan";
-
-function name(string) {
-  for (let i = 0; i < array.length; i++) {
+function mayuculas(textos) {
+  const letras = textos.split(" ");
+  for (let i = 0; i < letras.length; i++) {
+    letras[i] = letras[i][0].toUpperCase() + letras[i].substr(1);
   }
+  return letras.join(" ");
 }
+
+function main(params) {
+  console.log(alverris("1234"));
+  console.log(mayuculas("hola me llamo juan"));
+}
+main();
