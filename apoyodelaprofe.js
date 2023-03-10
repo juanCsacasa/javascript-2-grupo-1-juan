61b25aa18026ff081436d36fbc155fdffeffcf16
juan sacasa <juanksacasa@gmail.com>
2023-03-10 00:46:32 -0600

    part two

--- apoyodelaprofe.js
+++ apoyodelaprofe.js
@@ -1,140 +1,153 @@
 class Bookstore {
-    #bookstoreName;
-    #address;
-    #books;
-    #comics;
-    constructor(bookstoreName, address) {
-        this.#bookstoreName = bookstoreName;
-        this.#address = address;
-        this.#books = [];
-        this.#comics = [];
-    }
+  #bookstoreName;
+  #address;
+  #books;
+  #comics;
+  constructor(bookstoreName, address) {
+    this.#bookstoreName = bookstoreName;
+    this.#address = address;
+    this.#books = [];
+    this.#comics = [];
+  }
 
 
-    // getters
-    get getBooks() {
-        return this.#books;
-    }
+  // getters
+  get getBooks() {
+    return this.#books;
+  }
 
-    get getComics() {
-        return this.#comics;
-    }
+  get getComics() {
+    return this.#comics;
+  }
 
-    get getName() {
-        return this.#bookstoreName
-    }
+  get getName() {
+    return this.#bookstoreName
+  }
 
-    // setters
-    set setBookstoreName(name) {
-        this.#bookstoreName = name;
-    }
+  // setters
+  set setBookstoreName(name) {
+    this.#bookstoreName = name;
+  }
 
-    set setAddress(address) {
-        this.#address = address;
-    }
+  set setAddress(address) {
+    this.#address = address;
+  }
 
-    set setBooks(book) {
-        this.#books.push(book);
-    }
+  set setBooks(book) {
+    this.#books.push(book);
+  }
 
-    set setComics(comic) {
-        this.#comics.push(comic);
-    }
+  set setComics(comic) {
+    this.#comics.push(comic);
+  }
 }
 
 class Book {
-    #title;
-    #author;
-    #price;
-    #stock;
-    #year;
+  #title;
+  #author;
+  #price;
+  #stock;
+  #year;
 
-    constructor(title, author, price, stock, year) {
-        this.#title = title;
-        this.#author = author;
-        this.#price = price;
-        this.#stock = stock;
-        this.#year = year;
-    }
+  constructor(title, author, price, stock, year) {
+    this.#title = title;
+    this.#author = author;
+    this.#price = price;
+    this.#stock = stock;
+    this.#year = year;
+  }
 
-    getInfo() {
-        return {
-            title: this.#title,
-            auhor: this.#author,
-            price: this.#price,
-            year: this.#year,
-            stock: !this.#stock ? 'No hay ejemplares disponibles' : this.#stock
-        }
+  getInfo() {
+    return {
+      title: this.#title,
+      auhor: this.#author,
+      price: this.#price,
+      year: this.#year,
+      stock: !this.#stock ? 'No hay ejemplares disponibles' : this.#stock
     }
+  }
 
-    // setters
-    set setStock(stock) {
-        this.#stock = stock;
-    }
+  // setters
+  set setStock(stock) {
+    this.#stock = stock;
+  }
 
-    set setYear(year) {
-        this.#year = year;
-    }
+  set setYear(year) {
+    this.#year = year;
+  }
 }
 
 class Comic extends Book {
-    #illustrator;
-    #publisher;
-    #volume;
-    constructor(title, author, price, stock, year, illustrator, publisher, volume) {
-        super(title, author, price, stock, year);
-        this.#illustrator = illustrator;
-        this.#publisher = publisher;
-        this.#volume = volume;
-    }
+  #illustrator;
+  #publisher;
+  #volume;
+  constructor(title, author, price, stock, year, illustrator, publisher, volume) {
+    super(title, author, price, stock, year);
+    this.#illustrator = illustrator;
+    this.#publisher = publisher;
+    this.#volume = volume;
+  }
 
-    getInfo() {
-        const info = super.getInfo();
-        return {
-            ...info,
-            illustratorhi: this.#illustrator,
-            publisher: ts.#publisher,
-            volume: this.#volume
-        }
+  getInfo() {
+    const info = super.getInfo();
+    return {
+      ...info,
+      illustratorhi: this.#illustrator,
+      publisher: this.#publisher,
+      volume: this.#volume
     }
+  }
 }
 
 const bookstore1 = new Bookstore('Libreria 1', 'Calle 1');
 
 const addBook = () => {
-    const book = new Book('El principito', 'Antoine de Saint', 100, 10, 1943);
-    bookstore1.setBooks = book.getInfo();
-    console.log('Add book')
+  const book = new Book('El principito', 'Antoine de Saint', 100, 10, 1943);
+  bookstore1.setBooks = book.getInfo();
+  console.log('Add book')
 }
 
 const addComic = () => {
-    const comic = new Comic('Batman', 'bob kane', 200, 5, '1939', 'Bob kane', 'DC Comics', 1);
-    bookstore1.setComics = comic.getInfo();
-    console.log('Add Comic')
+  const comic = new Comic('Batman', 'bob kane', 200, 5, '1939', 'Bob kane', 'DC Comics', 1);
+  bookstore1.setComics = comic.getInfo();
+  console.log('Add Comic')
 }
 
 const showProducts = () => {
-    console.log('Books')
-    console.log(bookstore1.getBooks);
-
-    console.log('Comics')
-    console.log(bookstore1.getComics);
+  console.log('Books')
+  console.log(bookstore1.getBooks);
+  console.log('Comics')
+  console.log(bookstore1.getComics);
 }
 
-// const sidebar = document.getElementById('sidebar');
-// const menu = ['Show Books', 'Show Comics', 'Add book', 'Add Comic'];
+const sidebar = document.getElementById('sidebar');
+const menu = ['Show Books', 'Show Comics', 'Add book', 'Add Comic'];
+
+
+const createMenu = () => {
+  const menuList = document.createElement('ul');
+
+  menu.forEach(item => {
+    const listItem = document.createElement('li');
+    listItem.textContent = item;
+    menuList.appendChild(listItem);
+  });
+
+  sidebar.appendChild(menuList);
+}
+createMenu()
 
 // const printBookstoreName = () => {
-//     sidebar.insertAdjacentHTML(
-//         'afterbegin',
-//         `<h2 class="title">
+//   sidebar.insertAdjacentHTML(
+//     'afterbegin',
+//     `<h2 class="title">
 //         ${bookstore1.getName}
 //       </h2>`
-//     )
+//   )
 // }
 
 // printBookstoreName();
 
-  // ** TAREA **
-  // Crear una función que se encargué de crear el menu, iterando sobre la lista menu (línea 125)
-  // Debe generarse un element html tipo ul, y dentro de ese ul un li para cada elemento lista; Puede ver el ejemplo de lo que debe crear en el html 
+//    ** TAREA **
+//    Crear una función que se encargué de crear el menu, iterando sobre la lista menu (línea 125)
+//    Debe generarse un element html tipo ul, y dentro de ese ul un li para cada elemento lista; Puede ver el ejemplo de lo que debe crear en el html 
