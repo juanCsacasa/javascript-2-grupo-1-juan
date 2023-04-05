1c1ecd360c4c5a5f824b31789471d61ab0c74555
juan sacasa <juanksacasa@gmail.com>
2023-04-05 16:30:56 -0600

    pokedex

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
-const listChutemon = 'https://pokeapi.co/api/v2/pokemon?offset=110&limit=100';
+const listChutemon = 'https://pokeapi.co/api/v2/pokemon?offset=1&limit=802w';
 const nameChutemon = `https://pokeapi.co/api/v2/pokemon/{name}`
 const chutemonSelect = document.getElementById('pokemonSelect');
 const chutemonDetail = document.getElementById('pokemon-detail');
+const findChutemon = document.querySelector('form');
 
 fetch(listChutemon)
   .then(response => response.json())
@@ -41,5 +42,37 @@ chutemonSelect.addEventListener('change', () => {
         </ul>
       
       `;
+    });
+});
+
+findChutemon.addEventListener('submit', event => {
+  event.preventDefault();
+  const query = document.getElementById('searchChutemon').value;
+  fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
+    .then(response => response.json())
+    .then(data => {
+      chutemonDetail.innerHTML = `
+        <h2>${data.name}</h2>
+        <p class="chutemonNumero">${data.id}</p>
+        <img src="${data.sprites.front_default}" alt="${data.name}">
+        <p>Altura: ${data.height / 10} m</p>
+        <p>Peso: ${data.weight / 10} kg</p>
+        <h2>Tipo:</h2>
+        <ul>
+          ${data.types.map(type =>`<li><p>${type.type.name}</p></li>`).join('')}
+        </ul>
+        <h2>Habilidades:</h2>
+        <ul>
+          ${data.abilities.map(ability => `<li><p>${ability.ability.name}</p></li>`).join('')}
+        </ul>
+        <h2>Estadísticas:</h2>
+        <ul>
+          ${data.stats.map(stat => `<li><p>${stat.stat.name}: ${stat.base_stat}</p></li>`).join('')}
+        </ul>
+      
+      `;
+    })
+    .catch(error => {
+      console.error('Ha ocurrido un error al buscar', error);
     });
 });
